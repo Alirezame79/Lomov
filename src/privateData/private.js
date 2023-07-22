f1d72cc5d8f2abb7502f6ecba5152c25bc596bc6
Alirezame79 <alirezaghmathenglish@gmail.com>
2023-07-22 09:44:10 +0330

    Commit version 1.0.1

---
+++ src/privateData/private.js
@@ -0,0 +1,6 @@
+const headers = {
+    'X-RapidAPI-Key': '86daca9d1fmshb2eeb57f7e677d9p182f67jsncf9d222a6dd5',
+    'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+};
+
+export { headers };
